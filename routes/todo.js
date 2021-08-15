79f95760e408f103e5daa4ad05f7d16b2e59c179
furqaan ismail <ismailfurqaan@gmail.com>
2021-08-15 18:11:07 +0800

    edit file todo routes

--- routes/todo.js
+++ routes/todo.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send('tes todo');
+    res.send('tes todo lagi');
 });
 
 module.exports = router;
