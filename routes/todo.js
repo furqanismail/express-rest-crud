b8f80ae5f9ee054b37801adb5acc6a6274172afa
furqaan ismail <furqaanismail.labfik@umi.ac.id>
2021-08-15 18:28:00 +0800

    edit file todo routes

--- routes/todo.js
+++ routes/todo.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send('tes todo lagi 3');
+    res.send('tes todo lagi 4');
 });
 
 module.exports = router;
