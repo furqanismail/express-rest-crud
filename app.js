1eb1d79ab452e675295386473346dc1cc316e31d
furqaan ismail <furqaanismail.labfik@umi.ac.id>
2021-09-29 14:19:36 +0800

    clean file routes

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
 const todosRouter = require('./routes/todo');
 const usersRouter = require('./routes/user');
 
@@ -16,7 +15,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/todo', todosRouter);
 app.use('/user', usersRouter);
 
