27c58abcd31cb3593e67bd439b43f70422c785b7
furqaan ismail <furqaanismail.labfik@umi.ac.id>
2021-09-30 13:06:36 +0800

    add package security (helmet)

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const helmet = require('helmet')
 
 const todosRouter = require('./routes/todo');
 const usersRouter = require('./routes/user');
@@ -11,6 +12,7 @@ const verifyToken = require('./middlewares/verifyToken')
 
 const app = express();
 
+app.use(helmet())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
