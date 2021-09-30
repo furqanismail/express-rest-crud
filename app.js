9caae0507b47da39ae7a85f2d970374301f6e33a
furqaan ismail <furqaanismail.labfik@umi.ac.id>
2021-09-30 13:28:06 +0800

    add package security limiter

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const helmet = require('helmet')
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+    windowMs: 60 * 60 * 1000, // 1 hours
+    max: 10,
+    message: {
+        status: 'error',
+        message: 'Too many accounts created from this IP, please try again after 15 minutes'
+    }
+});
 
 const todosRouter = require('./routes/todo');
 const usersRouter = require('./routes/user');
@@ -20,6 +30,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/v1/todo', verifyToken, todosRouter);
-app.use('/api/v1/auth', usersRouter);
+app.use('/api/v1/auth', limiter, usersRouter);
 
 module.exports = app;
